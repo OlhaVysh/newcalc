4bab859c1fc76be2d5b185f3fea06000643d33ed
OlyaV <you@example.com>
2018-01-28 15:30:35 +0200

    first commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+/**
+ * Created by OlhaV on 28.01.2018.
+ */
+public class Calculator {
+
+    public int add(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+
+    public int sub(int firstNum, int secondNum) {
+        return firstNum - secondNum;
+    }
+
+    public int mult(int firstNum, int secondNum) {
+        return firstNum * secondNum;
+    }
+
+    public int div(int firstNum, int secondNum) {
+        return firstNum/secondNum;
+    }
+
+}
